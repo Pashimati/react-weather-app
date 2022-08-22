8ac74aae797ce0a2f218f144b2ee30bd8dd0ab06
Pashimati <Pashimatii@gmail.com>
2022-08-22 08:34:42 +0300

    add const key and uri weather-api

--- src/components/api.js
+++ src/components/api.js
@@ -6,4 +6,6 @@ export const geoApiOptions = {
     }
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '369bed60374e67401cc9bf44b9d92e06'
