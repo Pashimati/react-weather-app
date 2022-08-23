ed440f4c2eb9c56bc021c725a02d379fe0d3b4a2
Pashimati <Pashimatii@gmail.com>
2022-08-23 14:49:49 +0300

    full forecast add fix metric

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/search/Search";
 import CurrentWeather from "./components/current-weather/Current-weather";
 import {WEATHER_API_URL, WEATHER_API_KEY} from "./components/api";
 import {useState} from "react";
+import Forecast from "./components/forecast/Forecast";
 
 function App() {
 
@@ -12,9 +13,9 @@ function App() {
     const handleOnSearchChange = (searchData) => {
        const [lat, lon] = searchData.value.split(" ");
 
-       const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+       const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-       const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+       const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
        Promise.all([currentWeatherFetch, forecastFetch])
            .then(async (response) => {
@@ -32,7 +33,8 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather/>
+      <CurrentWeather data={currentWeather}/>
+        {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
